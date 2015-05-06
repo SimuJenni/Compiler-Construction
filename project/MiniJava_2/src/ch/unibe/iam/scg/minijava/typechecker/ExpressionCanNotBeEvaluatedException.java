4a1a6760b9cd34b97cf2118f09751e8517cef178
Manuel Leuenberger <manuel.leu@students.unibe.ch>
2015-05-06 11:12:28 +0200

    trying to fix typechecker

---
+++ project/MiniJava_2/src/ch/unibe/iam/scg/minijava/typechecker/ExpressionCanNotBeEvaluatedException.java
@@ -0,0 +1,7 @@
+package ch.unibe.iam.scg.minijava.typechecker;
+
+public class ExpressionCanNotBeEvaluatedException extends TypeCheckException {
+
+	private static final long serialVersionUID = 1L;
+
+}
